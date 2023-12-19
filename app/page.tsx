2991e9d93f9f0aa1a919b8b7dfba07526fc5761c
mgavillo <marie.gavillon@gmail.com>
2023-12-19 13:16:53 +0000

    router push in useeffect

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function HomePage() {
-  const router = useRouter();
-  router.push('/proposals');
+  useEffect(() => {
+    const router = useRouter();
+    router.push('/proposals');
+  }, []);
   return <></>;
 }
