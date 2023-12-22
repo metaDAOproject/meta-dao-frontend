7c4cad51590584f5ffd0da35b913255fc3e9009c
mgavillo <marie.gavillon@gmail.com>
2023-12-22 02:55:59 +0100

    lint & prettier

--- app/api/hackmd/route.ts
+++ app/api/hackmd/route.ts
@@ -1,18 +1,16 @@
-import { NextRequest, NextResponse } from 'next/server';
+// import { NextRequest, NextResponse } from 'next/server';
 // import { parse } from 'node-html-parser';
 
-export const GET = async (request: NextRequest) => {
+// export const GET = async (request: NextRequest) => {
+export const GET = async () => {
   // const { searchParams } = new URL(request.url);
   // const url = searchParams.get('url');
   // if (!url) return;
-
   // const response = await fetch(url, { method: 'GET' });
   // const text = await response.text();
   // const doc = parse(text).querySelector('#doc');
-
   // const markdown = doc?.childNodes[0].rawText;
   // if (!markdown) return;
-
   // const lines = markdown.split('\n');
   // const firstTitleIndex = lines.findIndex((line) => line.startsWith('#'));
   // const secondTitleIndex = lines.findIndex(
@@ -21,10 +19,8 @@ export const GET = async (request: NextRequest) => {
   // const thridTitleIndex = lines.findIndex(
   //   (line, i) => i > secondTitleIndex && line.startsWith('#'),
   // );
-
   // const title = lines[firstTitleIndex].replace('# ', '');
   // let description = lines.slice(secondTitleIndex + 1, thridTitleIndex).join('\n');
   // description = description.replace(/^\n+/, '');
-
   // return NextResponse.json({ title, description });
 };
