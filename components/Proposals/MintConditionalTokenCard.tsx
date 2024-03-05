ab6f818f45145409d9bb314d75579369a6be0a1c
LukasDeco <ldeconantsesznak@gmail.com>
2024-03-05 07:53:11 -0600

    Feat: use WS for wallet token balance updates

--- components/Proposals/MintConditionalTokenCard.tsx
+++ components/Proposals/MintConditionalTokenCard.tsx
@@ -1,5 +1,15 @@
 import { useCallback, useEffect, useState } from 'react';
-import { Button, Fieldset, Group, Text, TextInput, SegmentedControl, Loader, Stack, HoverCard } from '@mantine/core';
+import {
+    Button,
+    Fieldset,
+    Group,
+    Text,
+    TextInput,
+    SegmentedControl,
+    Loader,
+    Stack,
+    HoverCard,
+} from '@mantine/core';
 import numeral from 'numeral';
 import { BN } from '@coral-xyz/anchor';
 import { IconInfoCircle } from '@tabler/icons-react';
@@ -12,128 +22,122 @@ import useConditionalTokens from '@/hooks/useConditionalTokens';
 import { useProposalMarkets } from '@/contexts/ProposalMarketsContext';
 
 interface Balance {
-  token: Token;
-  symbol: string;
-  balanceSpot: BN;
-  balancePass: BN;
-  balanceFail: BN;
-  fetchUnderlying: () => Promise<void>;
-  fetchPass: () => Promise<void>,
-  fetchFail: () => Promise<void>;
-  finalize: PublicKey;
-  revert: PublicKey;
+    token: Token;
+    symbol: string;
+    balanceSpot: BN;
+    balancePass: BN;
+    balanceFail: BN;
+    finalize: PublicKey;
+    revert: PublicKey;
 }
 
 export function MintConditionalTokenCard() {
-  const sender = useTransactionSender();
-  const { mintTokensTransactions } = useProposal();
-  const { markets } = useProposalMarkets();
-  const [mintAmount, setMintAmount] = useState<number>();
-  const [isMinting, setIsMinting] = useState(false);
-  if (!markets) return null;
+    const sender = useTransactionSender();
+    const { mintTokensTransactions } = useProposal();
+    const { markets } = useProposalMarkets();
+    const [mintAmount, setMintAmount] = useState<number>();
+    const [isMinting, setIsMinting] = useState(false);
+    if (!markets) return null;
 
-  const { metaToken, usdcToken } = useConditionalTokens();
+    const { metaToken, usdcToken } = useConditionalTokens();
 
-  const [token, setToken] = useState<Balance | undefined>();
+    const [token, setToken] = useState<Balance | undefined>();
 
-  useEffect(() => {
-    setToken((prev) => {
-      if (!prev) return metaToken;
-      return prev.symbol === 'META' ? metaToken : usdcToken;
-    }
-    );
-  }, [metaToken, usdcToken]);
+    useEffect(() => {
+        setToken((prev) => {
+            if (!prev) return metaToken;
+            return prev.symbol === 'META' ? metaToken : usdcToken;
+        });
+    }, [metaToken, usdcToken]);
 
-  const updateSelectedToken = (e: string) => {
-    if (e === 'META') setToken(metaToken);
-    else if (e === 'USDC') {
-      setToken(usdcToken);
-    }
-  };
+    const updateSelectedToken = (e: string) => {
+        if (e === 'META') setToken(metaToken);
+        else if (e === 'USDC') {
+            setToken(usdcToken);
+        }
+    };
 
-  const handleMint = useCallback(async () => {
-    if (!mintAmount) return;
+    const handleMint = useCallback(async () => {
+        if (!mintAmount) return;
 
-    setIsMinting(true);
-    const fromBase = token?.symbol !== 'USDC';
-    try {
-      const txs = await mintTokensTransactions(mintAmount, fromBase);
+        setIsMinting(true);
+        const fromBase = token?.symbol !== 'USDC';
+        try {
+            const txs = await mintTokensTransactions(mintAmount, fromBase);
 
-      if (!txs) return;
+            if (!txs) return;
 
-      await sender.send(txs);
-      token?.fetchUnderlying();
-      token?.fetchFail();
-      token?.fetchPass();
-    } finally {
-      setIsMinting(false);
-    }
-  }, [mintTokensTransactions, sender, mintAmount, token]);
+            await sender.send(txs);
+        } finally {
+            setIsMinting(false);
+        }
+    }, [mintTokensTransactions, sender, mintAmount, token]);
 
-  return !token ?
-    (
-      <Group justify="center">
-        <Loader />
-      </Group>
+    return !token ? (
+        <Group justify="center">
+            <Loader />
+        </Group>
     ) : (
-      <Fieldset legend="Mint Conditional Tokens" miw="350px" w="100%" pos="relative">
-        <HoverCard position="top">
-          <HoverCard.Target>
-            <Group pos="absolute" top="-10px" right="0" justify="center" align="flex-start">
-              <IconInfoCircle strokeWidth={1.3} />
-            </Group>
-          </HoverCard.Target>
-          <HoverCard.Dropdown w="22rem">
-            <Stack>
-              <Text>
-                Conditional tokens are the tokens used to trade on conditional markets. You can mint
-                some by depositing $META or $USDC. These tokens will be locked up until the proposal
-                is finalized.
-              </Text>
-              <Text size="sm">
-                <Text span fw="bold">
-                  Pass tokens (pTokens){' '}
-                </Text>
-                are used to trade on the Pass Market
-              </Text>
-              <Text size="sm">
-                <Text span fw="bold">
-                  Fail tokens (fTokens){' '}
-                </Text>
-                are used to trade on the Fail Market.
-              </Text>
-            </Stack>
-          </HoverCard.Dropdown>
-        </HoverCard>
-        <SegmentedControl
-          style={{ marginTop: '10px' }}
-          color="#4e4e4e"
-          value={token.symbol}
-          className="label"
-          onChange={(e) =>
-            updateSelectedToken(e)
-          }
-          fullWidth
-          data={['META', 'USDC']}
-        />
-        <TextInput
-          label="Amount"
-          description={`Balance: ${numeral(token.balanceSpot?.uiAmountString || 0).format(NUMERAL_FORMAT)} $${token.token.symbol
-            }`}
-          placeholder="Amount to mint"
-          type="number"
-          onChange={(e) => setMintAmount(Number(e.target.value))}
-        />
+        <Fieldset legend="Mint Conditional Tokens" miw="350px" w="100%" pos="relative">
+            <HoverCard position="top">
+                <HoverCard.Target>
+                    <Group pos="absolute" top="-10px" right="0" justify="center" align="flex-start">
+                        <IconInfoCircle strokeWidth={1.3} />
+                    </Group>
+                </HoverCard.Target>
+                <HoverCard.Dropdown w="22rem">
+                    <Stack>
+                        <Text>
+                            Conditional tokens are the tokens used to trade on conditional markets.
+                            You can mint some by depositing $META or $USDC. These tokens will be
+                            locked up until the proposal is finalized.
+                        </Text>
+                        <Text size="sm">
+                            <Text span fw="bold">
+                                Pass tokens (pTokens){' '}
+                            </Text>
+                            are used to trade on the Pass Market
+                        </Text>
+                        <Text size="sm">
+                            <Text span fw="bold">
+                                Fail tokens (fTokens){' '}
+                            </Text>
+                            are used to trade on the Fail Market.
+                        </Text>
+                    </Stack>
+                </HoverCard.Dropdown>
+            </HoverCard>
+            <SegmentedControl
+                style={{ marginTop: '10px' }}
+                color="#4e4e4e"
+                value={token.symbol}
+                className="label"
+                onChange={(e) => updateSelectedToken(e)}
+                fullWidth
+                data={['META', 'USDC']}
+            />
+            <TextInput
+                label="Amount"
+                description={`Balance: ${numeral(token.balanceSpot?.uiAmountString || 0).format(
+                    NUMERAL_FORMAT,
+                )} $${token.token.symbol}`}
+                placeholder="Amount to mint"
+                type="number"
+                onChange={(e) => setMintAmount(Number(e.target.value))}
+            />
 
-        <Button
-          mt="md"
-          disabled={(mintAmount || 0) <= 0}
-          loading={isMinting}
-          onClick={handleMint}
-          fullWidth
-        >
-          Mint {mintAmount ? `${mintAmount} p${token.symbol} and ${mintAmount} f${token.symbol}` : ''}
-        </Button>
-      </Fieldset>
+            <Button
+                mt="md"
+                disabled={(mintAmount || 0) <= 0}
+                loading={isMinting}
+                onClick={handleMint}
+                fullWidth
+            >
+                Mint{' '}
+                {mintAmount
+                    ? `${mintAmount} p${token.symbol} and ${mintAmount} f${token.symbol}`
+                    : ''}
+            </Button>
+        </Fieldset>
     );
 }
--- contexts/BalancesContext.tsx
+++ contexts/BalancesContext.tsx
@@ -1,95 +1,141 @@
-import { createContext, useCallback, useContext, useState } from 'react';
+import { createContext, useCallback, useContext, useEffect, useState } from 'react';
 import { useConnection } from '@solana/wallet-adapter-react';
-import { PublicKey, TokenAmount } from '@solana/web3.js';
-import { getAssociatedTokenAddressSync } from '@solana/spl-token';
+import { AccountInfo, LAMPORTS_PER_SOL, PublicKey, TokenAmount } from '@solana/web3.js';
+import { AccountLayout, TOKEN_PROGRAM_ID, getAssociatedTokenAddressSync } from '@solana/spl-token';
 import { useQueryClient } from '@tanstack/react-query';
+import { BN } from '@coral-xyz/anchor';
 
 export const defaultAmount: TokenAmount = {
-  amount: '0.0',
-  decimals: 0.0,
-  uiAmount: 0.0,
+    amount: '0.0',
+    decimals: 0.0,
+    uiAmount: 0.0,
 };
 
-type Balances = { [token: string]: TokenAmount; };
+type Balances = { [token: string]: TokenAmount };
 
 export interface BalancesInterface {
-  balances: Balances;
-  fetchBalance: (mint: PublicKey) => Promise<TokenAmount>;
-  getBalance: (mint: PublicKey) => Promise<TokenAmount>;
+    balances: Balances;
+    fetchBalance: (mint: PublicKey) => Promise<TokenAmount>;
+    getBalance: (mint: PublicKey) => Promise<TokenAmount>;
 }
 
 export const balancesContext = createContext<BalancesInterface>({
-  balances: {},
-  fetchBalance: () => new Promise(() => { }),
-  getBalance: () => new Promise(() => { }),
+    balances: {},
+    fetchBalance: () => new Promise(() => {}),
+    getBalance: () => new Promise(() => {}),
 });
 
 export const useBalances = () => {
-  const context = useContext(balancesContext);
-  if (!context) {
-    throw new Error('useBalances must be used within a BalancesContextProvider');
-  }
-  return context;
+    const context = useContext(balancesContext);
+    if (!context) {
+        throw new Error('useBalances must be used within a BalancesContextProvider');
+    }
+    return context;
 };
 
 export function BalancesProvider({
-  children,
-  owner,
+    children,
+    owner,
 }: {
-  children: React.ReactNode;
-  owner?: PublicKey;
+    children: React.ReactNode;
+    owner?: PublicKey;
 }) {
-  const client = useQueryClient();
-  const { connection } = useConnection();
-  const [balances, setBalances] = useState<{ [token: string]: TokenAmount; }>({});
+    const client = useQueryClient();
+    const { connection } = useConnection();
+    const [balances, setBalances] = useState<{ [ata: string]: TokenAmount }>({});
+    const [websocketConnected, setWebsocketConnected] = useState<boolean>(false);
 
-  const fetchBalance = useCallback(
-    async (mint: PublicKey | string) => {
-      if (connection && owner) {
-        const account = getAssociatedTokenAddressSync(new PublicKey(mint.toString()), owner, true);
-        try {
-          const amount = await client.fetchQuery({
-            queryKey: [`getTokenAccountBalance-${account.toString()}-undefined`],
-            queryFn: () => connection.getTokenAccountBalance(account),
-            staleTime: 10_000,
-          });
-          setBalances((old) => ({
-            ...old,
-            [mint.toString()]: amount.value,
-          }));
-          return amount.value;
-        } catch (err) {
-          console.error(
-            `Error with this account fetch ${account.toString()} (owner: ${owner.toString()}, mint: ${mint?.toString()}), please review issue and solve.`,
-          );
-          return defaultAmount;
+    const fetchBalance = useCallback(
+        async (mint: PublicKey | string) => {
+            if (connection && owner) {
+                const ata = getAssociatedTokenAddressSync(
+                    new PublicKey(mint.toString()),
+                    owner,
+                    true,
+                );
+                try {
+                    const amount = await client.fetchQuery({
+                        queryKey: [`getTokenAccountBalance-${ata.toString()}-undefined`],
+                        queryFn: () => connection.getTokenAccountBalance(ata),
+                        staleTime: 10_000,
+                    });
+                    setBalances((old) => ({
+                        ...old,
+                        [ata.toString()]: amount.value,
+                    }));
+                    return amount.value;
+                } catch (err) {
+                    console.error(
+                        `Error with this account fetch ${ata.toString()} (owner: ${owner.toString()}, mint: ${mint?.toString()}), please review issue and solve.`,
+                    );
+                    return defaultAmount;
+                }
+            } else {
+                return defaultAmount;
+            }
+        },
+        [connection, owner],
+    );
+
+    async function findTokenAccountsByOwner(walletPublicKey: PublicKey): Promise<PublicKey[]> {
+        const { value } = await connection.getTokenAccountsByOwner(walletPublicKey, {
+            programId: new PublicKey(TOKEN_PROGRAM_ID),
+        });
+
+        return value.map(({ pubkey }) => pubkey);
+    }
+
+    async function subscribeToTokenBalances() {
+        if (!owner) return;
+        const tokenAccountPubkeys = await findTokenAccountsByOwner(owner);
+
+        for (const pubkey of tokenAccountPubkeys) {
+            connection.onAccountChange(pubkey, (accountInfo: AccountInfo<Buffer>) => {
+                const accountData = AccountLayout.decode(accountInfo.data);
+
+                const dividedTokenAmount = new BN(accountData.amount) / new BN(LAMPORTS_PER_SOL);
+                const tokenVal: TokenAmount = {
+                    amount: dividedTokenAmount.toString(),
+                    decimals: 9,
+                    uiAmount: dividedTokenAmount,
+                    uiAmountString: dividedTokenAmount.toString(),
+                };
+
+                setBalances((old) => ({
+                    ...old,
+                    [pubkey.toString()]: tokenVal,
+                }));
+            });
         }
-      } else {
-        return defaultAmount;
-      }
-    },
-    [connection, owner],
-  );
 
-  const getBalance = useCallback(
-    async (mint: PublicKey | string) => {
-      if (Object.prototype.hasOwnProperty.call(balances, mint.toString())) {
-        return balances[mint.toString()];
-      }
-      return fetchBalance(mint);
-    },
-    [balances, fetchBalance],
-  );
+        setWebsocketConnected(true);
+    }
 
-  return (
-    <balancesContext.Provider
-      value={{
-        balances,
-        fetchBalance,
-        getBalance,
-      }}
-    >
-      {children}
-    </balancesContext.Provider>
-  );
+    useEffect(() => {
+        if (!websocketConnected && owner) {
+            subscribeToTokenBalances();
+        }
+    }, [owner]);
+
+    const getBalance = useCallback(
+        async (mint: PublicKey | string) => {
+            if (Object.prototype.hasOwnProperty.call(balances, mint.toString())) {
+                return balances[mint.toString()];
+            }
+            return fetchBalance(mint);
+        },
+        [balances, fetchBalance],
+    );
+
+    return (
+        <balancesContext.Provider
+            value={{
+                balances,
+                fetchBalance,
+                getBalance,
+            }}
+        >
+            {children}
+        </balancesContext.Provider>
+    );
 }
